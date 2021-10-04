d0ddf093333c61fcec372f6eb8f7d13077937520
Gnabriel <elleb@kth.se>
2021-10-04 17:19:41 +0200

    Finished part 8-b until exercise 8.10

--- part8/library-frontend/src/index.js
+++ part8/library-frontend/src/index.js
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import {
   ApolloClient,
   ApolloProvider,
   HttpLink,
   InMemoryCache,
-  gql,
 } from "@apollo/client";
 
 const client = new ApolloClient({
@@ -16,22 +14,6 @@ const client = new ApolloClient({
   link: new HttpLink({
     uri: "http://localhost:4000",
   }),
-});
-
-const query = gql`
-  query {
-    allBooks {
-      title
-      author
-      published
-      genres
-      id
-    }
-  }
-`;
-
-client.query({ query }).then((response) => {
-  console.log(response.data);
 });
 
 ReactDOM.render(
@@ -42,8 +24,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
